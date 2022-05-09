09ec7b6f978cea6dd69883886419c359c119829f
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-05-10 02:32:03 +0300

    add js files for test

---
+++ src/string.js
@@ -0,0 +1,8 @@
+const stringLength = string => {
+    const str = "Hello";
+    return str;
+}
+
+const reverseString = (string) => string.toLowerCase().split("").reverse.join("");
+
+module.exports = {stringLength, reverseString}
