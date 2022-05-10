a1b079000647b72f99c20e958cc64ea380aced4e
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-05-10 23:14:10 +0300

    deleted

--- src/string.js
+++
@@ -1,8 +0,0 @@
-const stringLength = string => {
-    const str = "Hello";
-    return str;
-}
-
-const reverseString = (string) => string.toLowerCase().split("").reverse.join("");
-
-module.exports = {stringLength, reverseString}
